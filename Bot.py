08eb83deabb1296bada05abca82d597f8e1bd570
LoLka212 <91520018+LoLka212@users.noreply.github.com>
2022-11-17 15:29:33 +0600

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,20 @@
+import discord
+import interactions
+from discord.ext import commands
+
+bot = interactions.Client(token)
+
+@bot.command(
+    name="hello",
+    description="hello, how are you?",
+    scope=896655023600185344,)
+async def my_first_command(ctx: interactions.CommandContext):
+    await ctx.send("Hi there!")
+
+@bot.command(
+    name="serverinfo",
+    description="server info",
+    scope=896655023600185344,)
+async def (ctx: interactions.CommandContext):
+	
+bot.start()
